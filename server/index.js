15b3955c8cfae10624e0e8511f59c7284ec8e33c
Bhushan Chavan <bhushanc2003@gmail.com>
2024-02-28 21:57:27 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import { Server } from "socket.io";
+
+const PORT=9000;
+
+const io=new Server(PORT,{
+    cors:{
+        origin:'http://localhost:3000',
+        methods:['GET','POST']
+
+    }
+
+});
+
+io.on('connection',socket=>{
+    socket.on('get-document',documentId =>{
+        const data="";
+        socket.join(documentId);
+        socket.emit('load-document',data);
+        socket.on('send-changes',delta=>{
+            socket.broadcast.to(documentId).emit('receive-changes',delta);
+        })
+
+    });
+   
+    console.log('connected');
+});
